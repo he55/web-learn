8f938faeca86e761f51944ff746cbaa740e7fb68
he55 <40764272+he55@users.noreply.github.com>
2025-07-19 00:56:55 +0800

    Update HomeView.vue

--- document-project/src/views/HomeView.vue
+++ document-project/src/views/HomeView.vue
@@ -12,6 +12,9 @@ const loadData = async () => {
   doc.value = data
   html.value = md.render(data.output)
 }
+const reload = () => {
+  location.reload()
+}
 onMounted(() => {
   loadData()
 })
@@ -19,7 +22,27 @@ onMounted(() => {
 
 <template>
   <div class="container">
-    <nav><div>123</div></nav>
+    <nav>
+      <div class="title">
+        <p>病人列表</p>
+        <button class="reload" @click="reload">刷新</button>
+      </div>
+      <input type="search" placeholder="搜索" class="search" />
+      <div class="persons-wrapper">
+        <ul class="persons">
+          <li>12345</li>
+          <li>12345</li>
+          <li class="active">12345</li>
+          <li>12345</li>
+          <li>12345</li>
+          <li>12345</li>
+          <li>12345</li>
+          <li>12345</li>
+          <li>12345</li>
+          <li>12345</li>
+        </ul>
+      </div>
+    </nav>
     <header>
       <p>
         <strong>姓名：</strong>{{ doc.patient_name }} &nbsp;&nbsp;&nbsp;&nbsp;
@@ -41,21 +64,58 @@ onMounted(() => {
   height: 100%;
   padding: 10px;
   display: grid;
-  grid-template-areas:
-    'nav header'
-    'nav main';
+  grid-template-columns: 200px 1fr;
+  grid-template-rows: auto 1fr;
   gap: 10px;
-  background-color: rgb(242, 244, 246);
+  background-color: #f2f3f5;
 }
 nav {
-  grid-area: nav;
+  grid-row: 1/3;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
   width: 200px;
   padding: 10px;
   background-color: white;
   border-radius: 10px;
+
+  .title {
+    display: flex;
+    justify-content: space-between;
+  }
+  .reload {
+    padding: 2px 8px;
+  }
+  .search {
+    padding: 2px;
+  }
+  .persons-wrapper {
+    overflow-y: auto;
+  }
+  .persons {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding-right: 5px;
+    list-style-type: none;
+
+    li {
+      height: 100px;
+      padding: 5px;
+      border-radius: 10px;
+      background-color: #f2f3f5;
+    }
+    li.active {
+      color: #1e80ff;
+      background-color: #eaf2ff;
+    }
+    li:hover {
+      color: #1e80ff;
+    }
+  }
 }
 header {
-  grid-area: header;
   padding: 10px 15px;
   font-size: 20px;
   align-content: center;
@@ -63,7 +123,6 @@ header {
   border-radius: 10px;
 }
 main {
-  grid-area: main;
   overflow: auto;
   padding: 10px 25px;
   flex: 1;
