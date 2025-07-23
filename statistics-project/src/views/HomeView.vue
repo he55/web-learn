04f7c9ff658e4c20a164aea1d050421196931312
he55 <40764272+he55@users.noreply.github.com>
2025-07-23 23:25:05 +0800

    create statistics-project

---
+++ statistics-project/src/views/HomeView.vue
@@ -0,0 +1,150 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <table border="1" cellspacing="0">
+      <thead>
+        <tr>
+          <td colspan="11">昆明博骨风湿病中西医结合医院一病区工作日志</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td colspan="2">日期</td>
+          <td>入院</td>
+          <td colspan="2">出院</td>
+          <td colspan="2">手术</td>
+          <td colspan="2">危重</td>
+          <td>值班医生</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td rowspan="5">&nbsp;</td>
+          <td>今日</td>
+          <td>明日</td>
+          <td>今日</td>
+          <td>明日</td>
+          <td>病危</td>
+          <td>病重</td>
+          <td>夜班护士</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">人数</td>
+          <td rowspan="4">&nbsp;</td>
+          <td rowspan="4">&nbsp;</td>
+          <td rowspan="4">&nbsp;</td>
+          <td rowspan="4">&nbsp;</td>
+          <td rowspan="4">&nbsp;</td>
+          <td rowspan="4">&nbsp;</td>
+          <td>主班护士</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td>原有</td>
+          <td>现有</td>
+          <td>责1护士</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td rowspan="2">&nbsp;</td>
+          <td rowspan="2">&nbsp;</td>
+          <td>责2护士</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td>责3护士</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td rowspan="4">血压监测</td>
+          <td>Bid</td>
+          <td colspan="3">&nbsp;</td>
+          <td rowspan="6">血糖监测</td>
+          <td colspan="2" rowspan="6">&nbsp;</td>
+          <td rowspan="2">引流</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>Tid</td>
+          <td colspan="3">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>Q6h</td>
+          <td colspan="3">&nbsp;</td>
+          <td rowspan="2">吸氧</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>Q8h</td>
+          <td colspan="3">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>防跌倒坠床</td>
+          <td colspan="4">&nbsp;</td>
+          <td rowspan="2">冰敷</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>防压疮</td>
+          <td colspan="4">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>24h出入量</td>
+          <td colspan="3">&nbsp;</td>
+          <td>引流（VSD）</td>
+          <td colspan="3">&nbsp;</td>
+          <td>引流（伤口）</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>自体血</td>
+          <td colspan="3">&nbsp;</td>
+          <td>免疫吸附</td>
+          <td colspan="3">&nbsp;</td>
+          <td>关节离子</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>810</td>
+          <td colspan="3">&nbsp;</td>
+          <td>痛风脉冲</td>
+          <td colspan="3">&nbsp;</td>
+          <td>低频超声</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td>氦氖激光</td>
+          <td colspan="3">&nbsp;</td>
+          <td>中药封包</td>
+          <td colspan="3">&nbsp;</td>
+          <td>中药溻渍</td>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<style>
+.container {
+  box-sizing: border-box;
+  height: 100%;
+  padding: 8px;
+}
+table {
+  width: 100%;
+  height: 100%;
+  table-layout: fixed;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+}
+thead {
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
