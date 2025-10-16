9eff05086efec2c8be55704f268e4ea494266ff8
he55 <40764272+he55@users.noreply.github.com>
2025-10-16 22:39:25 +0800

    add my-app2

---
+++ expo-projects/my-app2/Home.js
@@ -0,0 +1,171 @@
+import { useEffect, useState } from 'react'
+import { Image, Modal, Platform, StyleSheet, Text, ToastAndroid, View } from 'react-native'
+import Settings from './Settings'
+import { useVideoPlayer, VideoView } from 'expo-video'
+import { getData, getConfig, loadConfig } from './utils'
+
+const bgImg = require('./assets/bg.jpg')
+
+export default function Home() {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [label1Text, setLabel1Text] = useState('')
+  const [label2Text, setLabel2Text] = useState('')
+  const [label3Text, setLabel3Text] = useState('')
+
+  const [bgImage, setBgImage] = useState(bgImg)
+
+  const player = useVideoPlayer('', (player) => {
+    player.loop = true
+    player.muted = true
+    player.play()
+  })
+
+  const setup = async () => {
+    try {
+      const config = await loadConfig()
+      // setBgImage({ uri: config.backgroundImageUrl ?? '' })
+      setLabel1Text(config.name ?? '')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  let videoSource = ''
+  const updateUI = (config) => {
+    if (bgImage !== config.backgroundImageUrl) {
+      // setBgImage({ uri: config.backgroundImageUrl })
+    }
+
+    if (label1Text !== config.name) {
+      setLabel1Text(config.name)
+    }
+
+    if (player.muted !== config.videoMuted) {
+      player.muted = config.videoMuted
+    }
+
+    if (videoSource !== config.videoUrl) {
+      videoSource = config.videoUrl
+      player.replaceAsync(config.videoUrl)
+    }
+  }
+
+  useEffect(() => {
+    setup()
+
+    let config
+    let timeoutId
+    let timeoutId2
+    let timeoutId3
+
+    const playerWatchDog = async () => {
+      console.log(new Date(), 'playerWatchDog')
+
+      if (videoSource) {
+        player.replay()
+      }
+
+      timeoutId3 = setTimeout(playerWatchDog, 60_000 * 10)
+    }
+    timeoutId3 = setTimeout(playerWatchDog, 60_000 * 10)
+
+    const getConfigTick = async () => {
+      console.log(new Date(), 'getConfig')
+      try {
+        config = await getConfig()
+        updateUI(config)
+      } catch (error) {
+        console.log(error)
+        if (Platform.OS === 'android') {
+          ToastAndroid.show('发送请求失败', ToastAndroid.LONG)
+        }
+      }
+      timeoutId2 = setTimeout(getConfigTick, 30_000)
+    }
+    getConfigTick()
+
+    const getDataTick = async () => {
+      console.log(new Date(), 'getData')
+      try {
+        if (config) {
+          const data = await getData(config.dataUrl)
+          setLabel2Text(data.text2)
+          setLabel3Text(data.text3)
+        }
+      } catch (error) {
+        console.log(error)
+        setLabel2Text('')
+        setLabel3Text('')
+      }
+      timeoutId = setTimeout(getDataTick, 5_000)
+    }
+    getDataTick()
+
+    return () => {
+      clearTimeout(timeoutId)
+      clearTimeout(timeoutId2)
+      clearTimeout(timeoutId3)
+    }
+  }, [])
+
+  return (
+    <>
+      <Modal visible={modalVisible}>
+        <Settings
+          onClose={() => {
+            setModalVisible(false)
+          }}
+        />
+      </Modal>
+      <View style={styles.container}>
+        <Image style={styles.bg} source={bgImage} />
+
+        <Text style={[styles.text, styles.label2]} onPress={() => setModalVisible(true)}>
+          {label2Text}
+        </Text>
+        <Text style={[styles.text, styles.label3]}>{label3Text}</Text>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bg: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'stretch',
+  },
+  text: {
+    width: 830,
+    height: 130,
+    position: 'absolute',
+    left: 125,
+    fontSize: 50,
+    fontWeight: 'bold',
+    lineHeight: 130,
+    textAlign: 'center',
+    backgroundColor: 'green',
+    opacity: 0.5,
+  },
+  label1: {
+    bottom: 840,
+  },
+  label2: {
+    bottom: 1040,
+    fontSize: 70,
+  },
+  label3: {
+    bottom: 110,
+    height: 820,
+  },
+  video: {
+    position: 'absolute',
+    left: 70,
+    bottom: 75,
+    width: 940,
+    height: 530,
+  },
+})
---
+++ expo-projects/my-app2/utils.js
@@ -0,0 +1,66 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export const loadConfig = async () => {
+  const json = await AsyncStorage.getItem('config')
+  if (!json) {
+    return null
+  }
+
+  return JSON.parse(json)
+}
+
+export const getConfig = async () => {
+  const url = await AsyncStorage.getItem('url')
+  if (!url) {
+    throw new Error('config url is not empty')
+  }
+
+  const res = await fetch(url, { cache: 'no-cache' })
+  if (!res.ok) {
+    throw new Error('get config failed')
+  }
+
+  const config = await res.json()
+  config.backgroundImageUrl = new URL(config.backgroundImageUrl, url).href
+  config.videoUrl = new URL(config.videoUrl, url).href
+  config.dataUrl = new URL(config.dataUrl, url).href
+
+  const json = JSON.stringify(config)
+
+  await AsyncStorage.setItem('config', json)
+
+  return config
+}
+
+export const getData = async (url) => {
+  const data = {
+    text2: '',
+    text3: '',
+  }
+
+  const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error('get data failed')
+  }
+
+  const list = await res.json()
+  if (!list.length) {
+    return data
+  }
+
+  const a = list.find((x) => x.state === 1)
+  if (a) {
+    data.text2 = `${a.number} ${a.name}`
+  }
+
+  /** @type {[]} */
+  const b = list.filter((x) => x.state === 0)
+  if (b.length) {
+    data.text3 = b
+      .slice(0, 6)
+      .map((x) => `${x.number} ${x.name}`)
+      .join('\n')
+  }
+
+  return data
+}
