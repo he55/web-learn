76287f893688236dbb733816372b9dbf089b66ef
he55 <40764272+he55@users.noreply.github.com>
2025-11-07 01:04:15 +0800

    Update index.ts

--- his-server-update/index.ts
+++ his-server-update/index.ts
@@ -9,10 +9,13 @@ main(process.argv.splice(2))
 
 function help() {
   console.log(`usage: ux package list
-       ux update <server> <version>`)
+       ux update <server> <version>
+       ux start <server>
+       ux stop <server>
+       ux restart <server>`)
 }
 
-function bin_cmd(subargs: string[]) {
+function package_cmd(subargs: string[]) {
   const [c] = subargs
   if (c === 'list') {
     const bins = fs.globSync('packages/*/')
@@ -25,6 +28,26 @@ function bin_cmd(subargs: string[]) {
   } else {
     help()
   }
+}
+
+function getServerFiles(serverName: string) {
+  const pattern = serverName === 'all' ? '*' : serverName
+
+  let configFilePaths = fs.globSync(`${serverPrefixName}${pattern}/app.xml`)
+  if (configFilePaths.length === 0) {
+    throw new Error('not found server')
+  }
+
+  const hasItem = excludeServerNames?.length > 0
+  if (hasItem) {
+    configFilePaths = configFilePaths.filter((x) => !excludeServerNames.some((y) => x.includes(y)))
+
+    if (configFilePaths.length === 0) {
+      throw new Error('not found server')
+    }
+  }
+
+  return configFilePaths
 }
 
 async function update_cmd(subargs: string[]) {
@@ -40,20 +63,9 @@ async function update_cmd(subargs: string[]) {
     throw new Error('bin not exists')
   }
 
-  const pattern = server === 'all' ? '*' : server
-
-  const configFilePaths = fs.globSync(`${serverPrefixName}${pattern}/app.xml`)
-  if (configFilePaths.length === 0) {
-    throw new Error('not found server')
-  }
-
-  const hasItem = excludeServerNames?.length > 0
+  const configFilePaths = getServerFiles(server!)
 
   for (const configPath of configFilePaths) {
-    if (hasItem && excludeServerNames.some((x) => configPath.includes(x))) {
-      continue
-    }
-
     const oldConfig = fs.readFileSync(configPath, 'utf8')
     const newConfig = oldConfig.replace(
       /<executable>(.+)<\/executable>/i,
@@ -63,6 +75,23 @@ async function update_cmd(subargs: string[]) {
 
     console.log(`winsw restart ${configPath}`)
     await $`winsw restart ${configPath}`.quiet()
+  }
+}
+
+type WinswCmd = 'start' | 'stop' | 'restart'
+
+async function winsw_cmd(subargs: string[], cmd: WinswCmd) {
+  const [server] = subargs
+  if (!server) {
+    help()
+    return
+  }
+
+  const configFilePaths = getServerFiles(server)
+
+  for (const configPath of configFilePaths) {
+    console.log(`winsw ${cmd} ${configPath}`)
+    await $`winsw ${cmd} ${configPath}`.quiet()
   }
 }
 
@@ -76,9 +105,15 @@ function main(args: string[]) {
   if (subcmd === 'help') {
     help()
   } else if (subcmd === 'package') {
-    bin_cmd(subargs)
+    package_cmd(subargs)
   } else if (subcmd === 'update') {
     update_cmd(subargs)
+  } else if (subcmd === 'start') {
+    winsw_cmd(subargs, 'start')
+  } else if (subcmd === 'stop') {
+    winsw_cmd(subargs, 'stop')
+  } else if (subcmd === 'restart') {
+    winsw_cmd(subargs, 'restart')
   } else {
     help()
   }
